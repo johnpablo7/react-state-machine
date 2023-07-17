315ec40d888c4803f2883ab03e912d00c09e8432
John Pablo <john.davis.2015.03@gmail.com>
2023-07-16 20:13:30 -0500

    first commit

---
+++ src/Machines/bookingMachine.jsx
@@ -0,0 +1,37 @@
+import { createMachine } from "xstate";
+
+const bookingMachine = createMachine({
+  predictableActionArguments: true,
+  id: "buy plane tickets",
+  initial: "home",
+  states: {
+    home: {
+      on: {
+        START: "search",
+      },
+    },
+    search: {
+      on: {
+        CONTINUE: "passengers",
+        CANCEL: "home",
+      },
+    },
+    passengers: {
+      on: {
+        DONE: "tickets",
+        CANCEL: "home",
+      },
+    },
+    tickets: {
+      on: {
+        BUY: "success",
+        CANCEL: "home",
+      },
+    },
+    success: {
+      on: {},
+    },
+  },
+});
+
+export default bookingMachine;
